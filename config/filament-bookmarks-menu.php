22be51c3f63b5da62ccff1dffac04523a91c9518
Thijmen Kort <101055765+ThijmenKort@users.noreply.github.com>
2022-12-21 11:43:23 +0100

    Update filament-bookmarks-menu.php

--- config/filament-bookmarks-menu.php
+++ config/filament-bookmarks-menu.php
@@ -5,16 +5,16 @@ return [
 
     // Icons to use for the icon you might add to your pages in
     // Filament Admin. Also the colors used for active and inactive.
-    'bookmark_icon' => 'heroicon-o-bookmark',
+    'bookmark_icon' => 'heroicon-o-star',
     'bookmark_class' => 'w-5 h-5 cursor-pointer text-gray-700 dark:text-gray-200',
-    'add_bookmark_icon' => 'heroicon-o-bookmark',
-    'remove_bookmark_icon' => 'heroicon-s-bookmark',
+    'add_bookmark_icon' => 'heroicon-o-star',
+    'remove_bookmark_icon' => 'heroicon-s-star',
     'add_bookmark_class' => 'w-6 h-6 cursor-pointer text-danger-700 dark:text-gray-200',
     'remove_bookmark_class' => 'w-6 h-6 cursor-pointer text-danger-700 dark:text-gray-200',
     'add_bookmarks_by_users' => true,
     'exclude_pages' => ['admin', 'bookmarks-menus'],
-    'notification_add_icon' => 'heroicon-o-bookmark',
-    'notification_remove_icon' => 'heroicon-o-bookmark',
+    'notification_add_icon' => 'heroicon-o-star',
+    'notification_remove_icon' => 'heroicon-o-star',
     'notification_add_color' => 'success',
     'notification_remove_color' => 'danger',
 ];
