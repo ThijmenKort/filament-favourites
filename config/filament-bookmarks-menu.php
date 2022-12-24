865b870e9b0a41b25052a556335c85598d85e4fd
Thijmen Kort <101055765+ThijmenKort@users.noreply.github.com>
2022-12-24 21:39:16 +0100

    Update filament-bookmarks-menu.php

--- config/filament-bookmarks-menu.php
+++ config/filament-bookmarks-menu.php
@@ -6,10 +6,10 @@ return [
     // Icons to use for the icon you might add to your pages in
     // Filament Admin. Also the colors used for active and inactive.
     'bookmark_icon' => 'heroicon-o-star',
-    'bookmark_class' => 'w-5 h-5 cursor-pointer text-gray-700 dark:text-gray-200',
+    'bookmark_class' => 'w-5 h-5 cursor-pointer text-gray-500 dark:text-gray-200',
     'add_bookmark_icon' => 'heroicon-o-star',
     'remove_bookmark_icon' => 'heroicon-s-star',
-    'add_bookmark_class' => 'w-6 h-6 cursor-pointer text-danger-700 dark:text-gray-200',
+    'add_bookmark_class' => 'w-6 h-6 cursor-pointer text-danger-500 dark:text-gray-200',
     'remove_bookmark_class' => 'w-6 h-6 cursor-pointer text-danger-700 dark:text-gray-200',
     'add_bookmarks_by_users' => true,
     'exclude_pages' => ['admin', 'bookmarks-menus'],
